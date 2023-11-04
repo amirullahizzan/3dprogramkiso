78d56a101926a344a3f54ab4b370152a17c9cba7
Zan <38888951+amirullahizzan@users.noreply.github.com>
2023-11-04 19:30:49 +0900

    Add files via upload

---
+++ 20231104/3D 最新 (modified file from Eto)/source/game.cpp
@@ -0,0 +1,255 @@
+﻿//===========================================================================
+//!	@file	game.cpp
+//!	@brief	ゲームメインループ
+//===========================================================================
+
+//                        変数
+std::shared_ptr<Texture> texture;   //!< テクスチャ
+
+//---------------------------------------------------------------------------
+//	初期化
+//!	@retval	true	正常終了    	(成功)
+//!	@retval	false	エラー終了	(失敗)
+//---------------------------------------------------------------------------
+bool GAME_setup()
+{
+    glClearColor(0.25f, 0.25f, 0.25f, 1.0f);   // 画面クリアカラーの設定
+    glClearDepth(1.0f);                        // Ｚバッファの初期化値
+    glEnable(GL_DEPTH_TEST);                   // Ｚバッファを有効にする
+
+    //----------------------------------------------------------
+    // テクスチャを読み込む
+    //----------------------------------------------------------
+    texture = LoadTexture("data/sample.tga");
+    if(!texture) {return false;}
+
+    return true;
+}
+
+//---------------------------------------------------------------------------
+//	更新
+//---------------------------------------------------------------------------
+void GAME_update()
+{
+    //pointer lesson
+    int  a = 10;
+    int* p = &a;
+    int**   p2 = &p;
+    int***  p3 = &p2;
+    int**** p4 = &p3;
+
+    //int b = ****p4; 
+    int b = *((*(*(*p4))));
+    // 
+    //-------------------------------------------------------------
+    // 座標更新
+    //-------------------------------------------------------------
+    glLoadIdentity();
+
+    if constexpr(true) {
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        gluPerspective(45.0f, 16.0f / 9.0f, 0.1f, 1000.0f);
+        gluLookAt(10.0f,    // x    カメラの位置(eye position)
+                  5.0f,    // y
+                  10.0f,   // z
+                  0.0f,    // x    注視点(look at)
+                  0.0f,    // y
+                  0.0f,    // z
+                  0.0f,    // x    世界の上方向のベクトル(up)
+                  1.0f,    // y
+                  0.0f);   // z
+
+        // もとに戻す
+        glMatrixMode(GL_MODELVIEW);
+    }
+
+    //-------------------------------------------------------------
+    // 描画
+    //-------------------------------------------------------------
+
+    //---- 画面クリア
+    // 描画の開始のためにバックバッファとＺバッファを初期化します
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    //-------------------------------------------------
+    //グリッドの描画
+    //-------------------------------------------------
+    
+    //定数
+    constexpr float SIZE = 64.0f;
+
+    glBegin(GL_LINES);
+    {
+        //x軸
+        glColor3ub(255, 0, 0);
+        glVertex3f(-SIZE, 0.0f, 0.0f);
+        glVertex3f(+SIZE, 0.0f, 0.0f);
+
+        //Y軸
+        glColor3ub(0, 255, 0);
+        glVertex3f(0.0f, -SIZE, 0.0f);
+        glVertex3f(0.0f, +SIZE, 0.0f);
+
+        //Z軸
+        glColor3ub(0, 0, 255);
+        glVertex3f(0.0f, 0.0f, -SIZE);
+        glVertex3f(0.0f, 0.0f, +SIZE);
+    }
+     glEnd();
+
+     
+
+     //定数
+//    constexpr float SIZE = 64.0f;
+     //グリッド線
+     glBegin(GL_LINES);
+    {
+        
+
+            ////X軸
+            //glColor3ub(0, 0, 0);
+            //glVertex3f(-SIZE, 0.0f, 0.0f );
+            //glVertex3f(+SIZE, 0.0f, 0.0f);
+
+            ////Y軸
+            //glColor3ub(0, 0, 0);
+            //glVertex3f(0.0f, -SIZE, 0.0f);
+            //glVertex3f(0.0f, +SIZE, 0.0f);
+
+            
+            //白色
+        //glColor3ub(0, 0, 0);
+        glColor3ub(255, 255, 255);
+        for(int i = -64; i <= 64; ++i) {
+            //   /の方向
+            glVertex3f(i, 0.0f, -SIZE);
+            glVertex3f(i, 0.0f, +SIZE);
+            //   \の方向
+            glVertex3f(-SIZE, 0.0f, i);
+            glVertex3f(+SIZE, 0.0f, i);
+        }
+        
+    }
+     glEnd();
+
+     static float x = 0.0f;
+     static float z = 0.0f;
+     static float y = 0.0f;
+ 
+     if(GetKeyState(VK_RIGHT) & 0x8000) {
+        x++;
+     }
+     if(GetKeyState(VK_LEFT) & 0x8000) {
+        x--;
+     }
+
+     if(GetKeyState(VK_UP) & 0x8000) {
+        z--;
+     }
+     if(GetKeyState(VK_DOWN) & 0x8000) {
+        z++;
+     }
+     
+
+     //-----------------------------------------------
+     // 四角形をテクスチャ付きで描画
+     //-----------------------------------------------
+     glLoadIdentity(); //元に戻す
+
+     SetTexture(texture);
+     //四角形を描画
+     glBegin(GL_TRIANGLE_STRIP);
+     {
+        glColor3ub(255, 255, 255);
+        //0        1
+        //+--------+
+        //|　　　／|
+        //| 　／   |
+        //|／      |
+        //+--------+
+        //2         3
+
+        glTexCoord2f(0.0f, 0.0f);
+        glVertex3f(-1, +1, 0); //左上
+
+        glTexCoord2f(1.0f, 0.0f);
+        glVertex3f(+1, +1, 0); //右上
+
+        glTexCoord2f(0.0f, 1.0f);
+        glVertex3f(-1, -1, 0); //左下
+
+        glTexCoord2f(1.0f, 1.0f);
+        glVertex3f(+1, -1, 0); //右上
+
+
+
+
+     }
+     glEnd();
+     //描画が終わったら空っぽに
+     SetTexture(nullptr);
+
+     //-----------------------------------------------
+     //ピラミッドの位置やスケール回転を指定
+     //-----------------------------------------------
+
+     
+     glLoadIdentity();//元に戻す
+     glTranslatef(x, y,z ); //移動
+     glScalef(4.0f, 4.0f, 4.0f);         //スケール
+     glRotatef(30.0f, 0.0f, 1.0f, 0.0f); //回転
+
+     glBegin(GL_TRIANGLES);
+     {
+         //底面
+        glColor3ub(255,255,0);
+        glVertex3f(-1, 0, -1);
+        glVertex3f(+1, 0, -1);
+        glVertex3f(-1, 0, +1);
+
+        glVertex3f(+1, 0, -1);
+        glVertex3f(-1, 0, +1);
+        glVertex3f(+1, 0, +1);
+
+        //奥側面
+        glColor3ub(255, 255, 255);
+        glVertex3f(-1,0,-1);
+        glVertex3f(+1,0,-1);
+        glVertex3f(0, 1, 0); 
+
+        //左側面
+        glColor3ub(0, 0, 255);
+        glVertex3f(-1, 0, -1);
+        glVertex3f(-1, 0, +1);
+        glVertex3f(0, 1, 0); 
+
+        //右側面
+        glColor3ub(0, 255, 0);
+        glVertex3f(+1, 0, +1);
+        glVertex3f(+1, 0, -1);
+        glVertex3f(0, 1, 0); 
+
+        //手前側面
+        glColor3ub(255, 0, 0);
+        glVertex3f(-1, 0, +1);
+        glVertex3f(+1, 0, +1);
+        glVertex3f(0, 1, 0); 
+     }
+     glEnd();
+    
+    // glLoadIdentity();//元に戻す
+     
+
+  
+}
+
+//---------------------------------------------------------------------------
+//	解放
+//---------------------------------------------------------------------------
+void GAME_cleanup()
+{
+    texture.reset();   // テクスチャを解放(手動)
+}
---
+++ 20231104/3D 最新 (modified file from Eto)/source/main.cpp
@@ -0,0 +1,149 @@
+﻿//===========================================================================
+//!	@file	main.cpp
+//!	@brief	アプリケーション開始
+//===========================================================================
+
+//---------------------------------------------------------------------------
+//!	ウィンドウプロシージャ
+//!	@param	[in]	hwnd	対象のウィンドウハンドル
+//!	@param	[in]	message	ウィンドウメッセージ
+//!	@param	[in]	wparam	パラメーター引数1
+//!	@param	[in]	lparam	パラメーター引数2
+//---------------------------------------------------------------------------
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
+{
+    switch(message) {
+    case WM_PAINT:   //---- 再描画リクエスト
+        {
+            PAINTSTRUCT          ps;
+            [[maybe_unused]] HDC hdc = BeginPaint(hwnd, &ps);
+
+            EndPaint(hwnd, &ps);
+        }
+        return 0;
+    case WM_DESTROY:   //---- ウィンドウ破棄
+        PostQuitMessage(0);
+        return 0;
+    default:   //---- その他
+        break;
+    }
+    //---- デフォルトのウィンドウプロシージャ
+    return DefWindowProc(hwnd, message, wparam, lparam);
+}
+
+//---------------------------------------------------------------------------
+//!	アプリケーション開始関数
+//---------------------------------------------------------------------------
+int APIENTRY WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+    const char* titleName = "OpenGL 3D";   // タイトルバーのテキスト
+    const char* className = "OpenGL";      // メインウィンドウクラス名
+
+    //-------------------------------------------------------------
+    // ウィンドウクラス登録
+    //-------------------------------------------------------------
+    WNDCLASSEX wcex{
+        .cbSize = sizeof(WNDCLASSEX),                   // 自分自身の構造体サイズ
+        .style  = CS_HREDRAW | CS_VREDRAW |             // 水平・垂直リサイズ時に再描画
+                 CS_DBLCLKS |                           // ダブルクリック有効化
+                 CS_OWNDC,                              // DC保持(OpenGLに必要)
+        .lpfnWndProc   = WndProc,                       // ウィンドウプロシージャー
+        .hInstance     = GetModuleHandle(nullptr),      // アプリケーションインスタンスハンドル
+        .hCursor       = LoadCursor(NULL, IDC_CROSS),   // カーソル（十字）
+        .hbrBackground = (HBRUSH)(COLOR_WINDOW + 1),    // 背景色（ブラシ）
+        .lpszClassName = className,                     // ウィンドウクラス名
+    };
+    RegisterClassEx(&wcex);
+
+    //-------------------------------------------------------------
+    // ウィンドウの位置を画面中央にセンタリング
+    //-------------------------------------------------------------
+#if 1
+    //---- ウィンドウ表示
+    SIZE windowSize{.cx = 1280, .cy = 720};
+    u32  style   = WS_OVERLAPPEDWINDOW;
+    u32  styleEx = 0;
+#else
+    //---- フルスクリーン表示
+    SIZE windowSize = {GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN)};
+    u32  style      = WS_POPUP;
+    u32  styleEx    = 0;
+#endif
+
+    //---- ウィンドウの大きさをウィンドウスタイルによって補正
+    RECT windowRect{.left = 0, .top = 0, .right = windowSize.cx, .bottom = windowSize.cy};   // ウィンドウ矩形
+    AdjustWindowRectEx(&windowRect, style, false, styleEx);
+
+    //---- ウィンドウ表示位置をセンタリング
+    s32 w = windowRect.right - windowRect.left;
+    s32 h = windowRect.bottom - windowRect.top;
+    s32 x = (GetSystemMetrics(SM_CXSCREEN) - w) / 2;
+    s32 y = (GetSystemMetrics(SM_CYSCREEN) - h) / 2;
+
+    //-------------------------------------------------------------
+    // ウィンドウ作成
+    //-------------------------------------------------------------
+    HWND hwnd = CreateWindowEx(styleEx,     // 拡張ウィンドウスタイル
+                               className,   // ウィンドウクラス名
+                               titleName,   // タイトル名 (任意)
+                               style,       // ウィンドウスタイル
+                               x,
+                               y,   // X,Y座標
+                               w,
+                               h,                          // 幅, 高さ
+                               nullptr,                    // 親ウィンドウのハンドル
+                               nullptr,                    // メニューのハンドル
+                               GetModuleHandle(nullptr),   // アプリケーションインスタンスハンドル
+                               nullptr);                   // WM_CREATEへの引数(任意)
+    //---- 作成失敗した場合は終了
+    if(hwnd == nullptr) {
+        return 0;
+    }
+
+    //---- 初期値はウィンドウ表示OFF状態のため、ここでON
+    ShowWindow(hwnd, SW_SHOW);
+    UpdateWindow(hwnd);
+
+    //=============================================================
+    // [OpenGL] 初期化
+    //=============================================================
+    if(OpenGL_setup(hwnd) == false) {
+        return 0;
+    }
+
+    //---- 【ゲーム】初期化
+    MSG message{};
+    if(GAME_setup() == true) {
+        //-------------------------------------------------------------
+        // メインメッセージループ:
+        //-------------------------------------------------------------
+        for(;;) {
+            if(PeekMessage(&message, nullptr, 0, 0, PM_NOREMOVE)) {
+                if(GetMessage(&message, nullptr, 0, 0) == 0) {
+                    break;
+                }
+                TranslateMessage(&message);
+                DispatchMessage(&message);
+            }
+            else {
+                //---- 【ゲーム】更新処理
+                GAME_update();
+
+                //=============================================================
+                // [OpenGL]	画面更新
+                //=============================================================
+                OpenGL_swapBuffer();
+            }
+        }
+    }
+
+    //---- 【ゲーム】解放
+    GAME_cleanup();
+
+    //=============================================================
+    // [OpenGL]	解放
+    //=============================================================
+    OpenGL_cleanup();   // OpenGLを解放
+
+    return (int)message.wParam;
+}
